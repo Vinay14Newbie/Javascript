d236c09a6f55278df8b6193c252d1ffd623629cd
Vinay14Newbie <wjust1984@gmail.com>
2023-12-02 19:46:39 +0530

    objects in js

---
+++ Basics_2/3_objects.js
@@ -0,0 +1,43 @@
+// singleton
+
+
+// object literals
+// Object.create  //it's constructor method
+
+const mySymb = Symbol("Key")  //this is how symbol is declared
+
+const jsUser = {
+    name : "vinay",
+    "full name" : "Vinay Duryodhan",
+    age : 19,
+    [mySymb] : "myKey2",   //this is how symbol is declared in objects
+    location : "chandrapur",
+    email : "vinayduryodhan510@gmail.com",
+    isLoggedIn : false,
+    lastLoginDays : ["Monday", "Saturday"]
+}
+
+console.log(jsUser.name);  
+console.log(jsUser["name"]);   //another way of accessing the objects value so this is best approach
+// let assume i've to access full name but it write in string but we can't access it through '.' so we, have to access it just like below or above
+console.log(jsUser["full name"]);
+console.log(jsUser[mySymb]);   //this is how symbol is accessed
+
+jsUser.name = "hitesh"  
+// Object.freeze(jsUser)  //Prevents the modification of existing property attributes and values, and prevents the addition of new properties.
+jsUser["name"] = "vinay"
+console.log(jsUser["name"]);
+
+jsUser.greeting = function(){
+    console.log("Hello guys");
+}
+console.log(jsUser.greeting);   //[Function (anonymous)]
+console.log(jsUser.greeting());  //Hello guys
+                                 // undefined
+
+
+jsUser.greetingTwo = function(){
+    console.log(`Hello guys, ${this.name}`);
+}
+
+console.log(jsUser.greetingTwo());
--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
 # Hey there
 
+Learning JS from Hitesh Choudhary
 
 To preview readme use ctrl + shift + v
