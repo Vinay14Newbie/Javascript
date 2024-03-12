bbb34019f2f84ff55594b6dcbe711e6ad993a107
Vinay14Newbie <wjust1984@gmail.com>
2024-03-12 18:58:09 +0530

    async

---
+++ 0_Revision/3-Async/1-script.js
@@ -0,0 +1,10 @@
+console.log("yash");
+setTimeout(()=>{
+    console.log("kunal");
+}, 0)
+console.log("vinay");
+console.log("jagdish");
+// output : yash
+//          vinay
+//          jagdish
+//          kunal
