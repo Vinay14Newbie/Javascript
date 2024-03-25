0d4cadf6689c0eb97b06ea5172bda44f11d6015a
Vinay14Newbie <wjust1984@gmail.com>
2024-03-25 20:01:46 +0530

    js revision complete

--- 0_Revision/3-Async/1-script.js
+++ 0_Revision/3-Async/1-script.js
@@ -1,10 +1,30 @@
-console.log("yash");
-setTimeout(()=>{
-    console.log("kunal");
-}, 0)
-console.log("vinay");
-console.log("jagdish");
+// console.log("yash");
+// setTimeout(()=>{
+//     console.log("kunal");
+// }, 0)
+// console.log("vinay");
+// console.log("jagdish");
 // output : yash
 //          vinay
 //          jagdish
 //          kunal
+
+
+
+
+// 
+let a = new Promise((res, reject) =>{
+    if(true){
+        return res()
+    }
+    else{
+        return reject()
+    }
+})
+a
+.then(()=>{
+    console.log("resolved");
+})
+.catch(()=>{
+    console.log("rejected");
+})
